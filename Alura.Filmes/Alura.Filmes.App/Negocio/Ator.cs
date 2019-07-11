8e4f12f34351c89e4df8bea595ec079543787f5d
deliasfrancisco <deliasfrancisco@gmail.com>
2019-07-10 22:29:30 -0300

    Segundo Projeto mais atual

---
+++ Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Alura.Filmes.App.Negocio
+{
+	[Table("actor")] /// notação table -> usada para o entity fazer o mapeamento do banco usando o metodo ator sem que ele tenha que ter o mesmo nome da tabela 
+	public class Ator
+	{
+		[Column("actor_id")] // notação column - associando o nome das colunas da tabela com as variaveis criadas no metodo
+		public int Id { get; set; }
+		[Column("first_name", TypeName = "varchar(45)")]
+		public string PrimeiroNome { get; set; }
+		[Column("last_name", TypeName = "varchar(45)")]
+		public string UltimoNome { get; set; }
+
+		public override string ToString()
+		{
+			return $"Ator({Id}): {PrimeiroNome} {UltimoNome}";
+		}
+	}
+}
--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-        }
+			using (var contexto = new AluraFilmesContexto())
+			{
+				foreach (var ator in contexto.Atores)
+				{
+					System.Console.WriteLine(ator);
+				}
+
+				System.Console.ReadLine();
+			}
+		}
     }
 }
